b420951ce44cd89cc95f0355255561b9b4791f8c
Luciano Belotto <DarthLuch@gmail.com>
2026-03-07 17:48:05 -0500

    1

--- 500 - post steps.sql
+++ 500 - post steps.sql
@@ -1,15 +1,11 @@
 
 
-Load Task cards: @'/Users/luch/Downloads/2026-03-04 - HA TC.sql'
+
+Hawaii: 
+Load Task cards: 
+@'/Users/luch/Warez/TraxDatabaseImport/hawaii/2026-03-04 - HA TC.sql';
 (TC data was not loaded because TC_XML field being too large)
-
-Set email-server in Settings, Profile Master, Servers-tab
-
-Assign user Luch to profiles: 'TRAX ADMIN', 'IT_ADMIN'
-
 ;
-
-@'/Users/luch/Warez/Dmp/2026-03-04 - Hawaiian/enable-fk-triggers.sql';
 
 
 
@@ -17,5 +13,4 @@ UPDATE TRAX_GLOBAL_FILES TGF
 SET TGF."SMTP_SERVER_ADDRESS" = '10.211.55.2'
 WHERE 1=1
 ;
-
-10.211.55.2
+commit;
